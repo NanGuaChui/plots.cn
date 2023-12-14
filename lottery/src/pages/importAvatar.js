41a7c1cfedfd87dcab49aec454a76a3e919735a6
ATRAPA\sgao <sgao@deloitte.com.cn>
2023-12-14 11:16:39 +0800

    123123

--- lottery/src/pages/importAvatar.js
+++ lottery/src/pages/importAvatar.js
@@ -1,18 +1,15 @@
 const jpgModules = import.meta.globEager("../assets/img/avatar/*.jpg");
 const pngModules = import.meta.globEager("../assets/img/avatar/*.png");
 
-const avatarList = {};
-let x = 0;
+const avatarList = {
+  default: "./logo.ico",
+};
 const modules = { ...jpgModules, ...pngModules };
 for (const path in modules) {
   const item = modules[path];
   const userName = path.split("/").pop().split(".")[0];
   avatarList[userName] = modules[path].default;
-
-  x++;
 }
-console.log(x);
-console.log(avatarList);
 
 export default (userName) => {
   return avatarList[userName] || avatarList["default"] || userName;

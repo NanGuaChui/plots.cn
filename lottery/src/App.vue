f76308acdb582e841b404712ec9a3ae287726286
sgao <sgao@deloitte.com.cn>
2025-01-08 17:42:21 +0800

    删除不再使用的文件和代码

--- lottery/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <Lottery msg="Vite + Vue" />
-</template>
-
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Lottery from "./pages/lottery.vue";
-</script>
